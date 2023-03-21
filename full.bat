91f76be5a5fb6272b59f03409cc1102671c00eb7
sammtcbn <sammtcbn@gmail.com>
2023-03-22 00:41:33 +0800

    add a sandbox for rich software

---
+++ full.bat
@@ -0,0 +1,39 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+
+rem init
+call utils\is-in-wsb.bat
+if %errorlevel% equ 0 (
+  call utils\folder-create.bat C:\wsb-tmp
+  call utils\logw.bat start time - %date% %time%
+)
+
+call utils\command-prompt-encoding-utf8.bat
+call utils\wait-network-ready.bat
+
+rem chocolatey
+call utils\scoop-install.bat
+
+rem =======================================================
+rem Custom begin
+rem =======================================================
+
+call utils\scoop-pkg-ins.bat 7zip
+call utils\scoop-pkg-ins.bat notepadplusplus
+call utils\scoop-pkg-ins.bat googlechrome
+call utils\scoop-pkg-ins.bat git
+
+rem =======================================================
+rem Custom end
+rem =======================================================
+
+
+rem finish
+call utils\is-in-wsb.bat
+if %errorlevel% equ 0 (
+  call utils\generate-ip.bat
+  call utils\generate-boot-ok.bat
+  call utils\logw.bat end time - %date% %time%
+)
