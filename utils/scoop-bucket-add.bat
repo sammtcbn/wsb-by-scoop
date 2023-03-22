b2d18f7c7eb103978e2dc2cf03e9a081484a40b3
sammtcbn <sammtcbn@gmail.com>
2023-03-23 00:40:19 +0800

    log for add bucket

--- utils/scoop-bucket-add.bat
+++ utils/scoop-bucket-add.bat
@@ -6,11 +6,11 @@ where /q scoop
 if /I %ERRORLEVEL% NEQ 0 (
 
 	if exist C:\Users\%UserName%\scoop\shims\scoop.cmd (
-        C:\Users\%UserName%\scoop\shims\scoop bucket add %bucketname%
+        C:\Users\%UserName%\scoop\shims\scoop bucket add %bucketname% >> C:\wsb-tmp\scoop-log.txt
 	) else (
         echo scoop not found >> C:\wsb-tmp\scoop-log.txt
 	)
 
 ) else (
-    scoop bucket add %bucketname%
+    scoop bucket add %bucketname% >> C:\wsb-tmp\scoop-log.txt
 )
